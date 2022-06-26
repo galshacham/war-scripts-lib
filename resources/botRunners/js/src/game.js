485fa2fac4ff4c48c3b9e082293017b18ee74df2
galshacham <gal.shacham@gmail.com>
2022-06-27 00:41:06 +0300

    Adding tests and basic javascript, now things are working nicely

--- resources/botRunners/js/src/game.js
+++ resources/botRunners/js/src/game.js
@@ -1,13 +1,46 @@
 class Game {
     side = -1
     mapData = {}
-    castles = {}
 
     constructor(jsonGameState, side) {
         const gameState = JSON.parse(jsonGameState);
-        this.mapData = gameState.mapData;
-        this.castles = gameState.gameObjects.map(castleData => new Castle({...castleData}));
+        this.mapData = new MapData(gameState.mapData.rows, gameState.mapData.cols, gameState.mapData.players);
+        this.gameObjects = gameState.gameObjects.map(gameObject => {
+            const {id, side, loc: {row, col}} = gameObject
+            switch (gameObject.objectType) {
+                case CASTLE_TYPE :
+                    return new Castle(id, side, new Loc(row, col), gameObject.creatingSoldierType)
+            }
+        });
         this.side = side;
+    }
+
+    getAllMyCastles() {
+        return this.gameObjects.filter(gameObject => gameObject instanceof Castle && gameObject.side === this.side)
+    }
+}
+
+const CASTLE_TYPE = "CASTLE";
+
+class MapData {
+    rows
+    cols
+    players
+
+    constructor(rows, cols, players) {
+        this.rows = rows
+        this.cols = cols
+        this.players = players
+    }
+}
+
+class Loc {
+    row
+    col
+
+    constructor(row, col) {
+        this.row = row
+        this.col = col
     }
 }
 
@@ -15,11 +48,13 @@ class Castle {
     id
     soldierType
     side
+    loc
 
-    constructor({id, soldierType, side}) {
+    constructor(id, side, loc, soldierType) {
         this.id = id;
         this.soldierType = soldierType;
         this.side = side;
+        this.loc = loc;
     }
 
     changeSoldierType(soldierType) {
@@ -32,5 +67,7 @@ class Castle {
 
 module.exports = {
     Game,
-    Castle
+    Castle,
+    Loc,
+    MapData
 }
---
+++ resources/botRunners/js/test/game.test.js
@@ -0,0 +1,83 @@
+const {Loc, Castle, Game, MapData} = require("../src/game");
+
+const basicGameState = {
+    "mapData": {
+        "rows": 20,
+        "cols": 20,
+        "players": [
+            0,
+            1
+        ]
+    },
+    "gameObjects": [
+        {
+            "objectType": "CASTLE",
+            "side": 0,
+            "id": "0",
+            "creatingSoldierType": "MELEE",
+            "loc": {
+                "row": 1,
+                "col": 1
+            }
+        },
+        {
+            "objectType": "CASTLE",
+            "side": 1,
+            "id": "1",
+            "creatingSoldierType": "MELEE",
+            "loc": {
+                "row": 19,
+                "col": 19
+            }
+        },
+        {
+            "objectType": "CASTLE",
+            "side": -1,
+            "id": "2",
+            "creatingSoldierType": "MELEE",
+            "loc": {
+                "row": 1,
+                "col": 19
+            }
+        },
+        {
+            "objectType": "CASTLE",
+            "side": -1,
+            "id": "3",
+            "creatingSoldierType": "MELEE",
+            "loc": {
+                "row": 19,
+                "col": 1
+            }
+        }
+    ]
+}
+
+const expectedGameObject = {
+    mapData: new MapData(20, 20, [0, 1]),
+    gameObjects: [
+        new Castle("0", 0, new Loc(1, 1), "MELEE"),
+        new Castle("1", 1, new Loc(19, 19), "MELEE"),
+        new Castle("2", -1, new Loc(1, 19), "MELEE"),
+        new Castle("3", -1, new Loc(19, 1), "MELEE"),
+    ],
+    side: 1
+}
+expectedGameObject.__proto__ = Game.prototype
+
+const basicGameStringString = JSON.stringify(basicGameState)
+
+test("whenInitializingGameFromJsonState_shouldConstructNewGame", () => {
+    const game = new Game(basicGameStringString, 1);
+
+    expect(game).toStrictEqual(expectedGameObject)
+})
+
+test("givenValidGame_whenGettingAllMyCastles_shouldReturnOnlyMyCastles", () => {
+    const game = new Game(basicGameStringString, -1);
+    const expectedCastles = [
+        new Castle("2", -1, new Loc(1, 19), "MELEE"),
+        new Castle("3", -1, new Loc(19, 1), "MELEE")]
+    
+    expect(game.getAllMyCastles()).toEqual(expectedCastles)
+})
