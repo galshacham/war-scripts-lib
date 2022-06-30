e2c2442ed7c536097cd448aee7de17ebe59ff59b
galshacham <gal.shacham@gmail.com>
2022-06-30 08:50:47 +0300

    bot change

--- testResources/botRunner/demoJsCode.js
+++ testResources/botRunner/demoJsCode.js
@@ -1,7 +1,9 @@
 const {Game} = require("../../resources/botRunners/js/src/game")
 
 function doTurn(game) {
-    // game.castles[0].changeSoldierType("RANGED")
+    if (game instanceof Game) {
+        // game.getAllMyCastles()[0].changeSoldierType("RANGED")
+    }
 
     return game;
 }
