24dd2bc3402d9a0c957645e655b7ff035dc7f21e
galshacham <gal.shacham@gmail.com>
2022-07-02 23:47:50 +0300

    Adding more basic tests

--- resources/botRunners/js/src/game.js
+++ resources/botRunners/js/src/game.js
@@ -1,84 +1,110 @@
 class Game {
-    player // int
-    mapData // MapData
-    gameObjects // GameObject[]
+  player; // int
+  mapData; // MapData
+  gameObjects; // GameObject[]
 
-    constructor(jsonGameState, player) {
-        const gameState = JSON.parse(jsonGameState);
-        this.mapData = new MapData(gameState.mapData.rows, gameState.mapData.cols, gameState.mapData.players);
-        this.gameObjects = gameState.gameObjects.map(gameObject => {
-            const {id, player, loc: {row, col}} = gameObject
-            switch (gameObject.objectType) {
-                case CASTLE_TYPE :
-                    return new Castle(id, player, new Loc(row, col), gameObject.creatingSoldierType)
-            }
-        });
-        this.player = player;
-    }
+  constructor(jsonGameState, player) {
+    const gameState = JSON.parse(jsonGameState);
+    this.mapData = new MapData(
+      gameState.mapData.rows,
+      gameState.mapData.cols,
+      gameState.mapData.players
+    );
+    this.gameObjects = gameState.gameObjects.map((gameObject) => {
+      const {
+        id,
+        player,
+        loc: { row, col },
+      } = gameObject;
+      switch (gameObject.objectType) {
+        case CASTLE_TYPE:
+          return new Castle(
+            id,
+            player,
+            new Loc(row, col),
+            gameObject.creatingSoldierType
+          );
+      }
+    });
+    this.player = player;
+  }
 
-    getAllMyCastles() {
-        return this.gameObjects.filter(gameObject => gameObject instanceof Castle && gameObject.player === this.player)
-    }
+  getAllMyCastles() {
+    return this.gameObjects.filter(
+      (gameObject) =>
+        gameObject instanceof Castle && gameObject.player === this.player
+    );
+  }
 }
 
 const CASTLE_TYPE = "CASTLE";
 
 class MapData {
-    rows // int
-    cols // int
-    players // int[]
+  rows; // int
+  cols; // int
+  players; // int[]
 
-    constructor(rows, cols, players) {
-        this.rows = rows
-        this.cols = cols
-        this.players = players
-    }
+  constructor(rows, cols, players) {
+    this.rows = rows;
+    this.cols = cols;
+    this.players = players;
+  }
 }
 
 class Loc {
-    row // int
-    col // int
+  row; // int
+  col; // int
 
-    constructor(row, col) {
-        this.row = row
-        this.col = col
-    }
+  constructor(row, col) {
+    this.row = row;
+    this.col = col;
+  }
+}
+
+class AbstractAction {}
+
+class ChangeSoldierTypeAction extends AbstractAction {
+  soldierType; // string
+
+  constructor(soldierType) {
+    super();
+    this.soldierType = soldierType;
+  }
 }
 
 class GameObject {
-    id // string
-    player // int
-    loc // Loc
+  id; // string
+  player; // int
+  loc; // Loc
+  action; // ?Action
 
-    constructor(id, player, loc) {
-        this.id = id;
-        this.player = player;
-        this.loc = loc;
-    }
+  constructor(id, player, loc) {
+    this.id = id;
+    this.player = player;
+    this.loc = loc;
+  }
 }
 
 class Castle extends GameObject {
-    soldierType // string
+  soldierType; // string
 
-    constructor(id, player, loc, soldierType) {
-        super(id, player, loc);
-        this.id = id;
-        this.soldierType = soldierType;
-        this.player = player;
-        this.loc = loc;
-    }
+  constructor(id, player, loc, soldierType) {
+    super(id, player, loc);
+    this.id = id;
+    this.soldierType = soldierType;
+    this.player = player;
+    this.loc = loc;
+  }
 
-    changeSoldierType(soldierType) {
-        this.actions.push({
-            actionType: "CHANGE_SOLDIER_TYPE",
-            soldierType
-        });
-    }
+  changeSoldierType(soldierType) {
+    this.action = new ChangeSoldierTypeAction(soldierType);
+  }
 }
 
 module.exports = {
-    Game,
-    Castle,
-    Loc,
-    MapData
-}
+  Game,
+  Castle,
+  Loc,
+  MapData,
+  ChangeSoldierTypeAction,
+};
--- resources/botRunners/js/test/game.test.js
+++ resources/botRunners/js/test/game.test.js
@@ -1,83 +1,93 @@
-const {Loc, Castle, Game, MapData} = require("../src/game");
+const { Loc, Castle, Game, MapData, ChangeSoldierTypeAction } = require("../src/game");
 
 const basicGameState = {
-    "mapData": {
-        "rows": 20,
-        "cols": 20,
-        "players": [
-            0,
-            1
-        ]
+  mapData: {
+    rows: 20,
+    cols: 20,
+    players: [0, 1],
+  },
+  gameObjects: [
+    {
+      objectType: "CASTLE",
+      player: 0,
+      id: "0",
+      creatingSoldierType: "MELEE",
+      loc: {
+        row: 1,
+        col: 1,
+      },
     },
-    "gameObjects": [
-        {
-            "objectType": "CASTLE",
-            "player": 0,
-            "id": "0",
-            "creatingSoldierType": "MELEE",
-            "loc": {
-                "row": 1,
-                "col": 1
-            }
-        },
-        {
-            "objectType": "CASTLE",
-            "player": 1,
-            "id": "1",
-            "creatingSoldierType": "MELEE",
-            "loc": {
-                "row": 19,
-                "col": 19
-            }
-        },
-        {
-            "objectType": "CASTLE",
-            "player": -1,
-            "id": "2",
-            "creatingSoldierType": "MELEE",
-            "loc": {
-                "row": 1,
-                "col": 19
-            }
-        },
-        {
-            "objectType": "CASTLE",
-            "player": -1,
-            "id": "3",
-            "creatingSoldierType": "MELEE",
-            "loc": {
-                "row": 19,
-                "col": 1
-            }
-        }
-    ]
-}
+    {
+      objectType: "CASTLE",
+      player: 1,
+      id: "1",
+      creatingSoldierType: "MELEE",
+      loc: {
+        row: 19,
+        col: 19,
+      },
+    },
+    {
+      objectType: "CASTLE",
+      player: -1,
+      id: "2",
+      creatingSoldierType: "MELEE",
+      loc: {
+        row: 1,
+        col: 19,
+      },
+    },
+    {
+      objectType: "CASTLE",
+      player: -1,
+      id: "3",
+      creatingSoldierType: "MELEE",
+      loc: {
+        row: 19,
+        col: 1,
+      },
+    },
+  ],
+};
 
 const expectedGameObject = {
-    mapData: new MapData(20, 20, [0, 1]),
-    gameObjects: [
-        new Castle("0", 0, new Loc(1, 1), "MELEE"),
-        new Castle("1", 1, new Loc(19, 19), "MELEE"),
-        new Castle("2", -1, new Loc(1, 19), "MELEE"),
-        new Castle("3", -1, new Loc(19, 1), "MELEE"),
-    ],
-    player: 1
-}
-expectedGameObject.__proto__ = Game.prototype
+  mapData: new MapData(20, 20, [0, 1]),
+  gameObjects: [
+    new Castle("0", 0, new Loc(1, 1), "MELEE"),
+    new Castle("1", 1, new Loc(19, 19), "MELEE"),
+    new Castle("2", -1, new Loc(1, 19), "MELEE"),
+    new Castle("3", -1, new Loc(19, 1), "MELEE"),
+  ],
+  player: 1,
+};
+expectedGameObject.__proto__ = Game.prototype;
 
-const basicGameStringString = JSON.stringify(basicGameState)
+const basicGameStringString = JSON.stringify(basicGameState);
 
 test("whenInitializingGameFromJsonState_shouldConstructNewGame", () => {
-    const game = new Game(basicGameStringString, 1);
+  const game = new Game(basicGameStringString, 1);
 
-    expect(game).toStrictEqual(expectedGameObject)
-})
+  expect(game).toStrictEqual(expectedGameObject);
+});
 
 test("givenValidGame_whenGettingAllMyCastles_shouldReturnOnlyMyCastles", () => {
-    const game = new Game(basicGameStringString, -1);
-    const expectedCastles = [
-        new Castle("2", -1, new Loc(1, 19), "MELEE"),
-        new Castle("3", -1, new Loc(19, 1), "MELEE")]
+  const game = new Game(basicGameStringString, -1);
+  const expectedCastles = [
+    new Castle("2", -1, new Loc(1, 19), "MELEE"),
+    new Castle("3", -1, new Loc(19, 1), "MELEE"),
+  ];
 
-    expect(game.getAllMyCastles()).toStrictEqual(expectedCastles)
-})
+  expect(game.getAllMyCastles()).toStrictEqual(expectedCastles);
+});
+
+test("givenCastle_whenChangingSoldierType_shouldAddCorrectActionToCastle", () => {
+  const castle = new Castle("2", -1, new Loc(1, 19), "MELEE");
+  const newSoldierType = "RANGED";
+
+  const expectedCastle = new Castle("2", -1, new Loc(1, 19), "MELEE");
+  expectedCastle.action = new ChangeSoldierTypeAction(newSoldierType);
+
+  castle.changeSoldierType(newSoldierType);
+
+  expect(castle).toStrictEqual(expectedCastle);
+});
