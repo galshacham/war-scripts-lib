f63788bda8d9c3354b0fcf546f026c204744543a
galshacham <gal.shacham@gmail.com>
2022-09-27 12:44:13 +0300

    Adding comment about running turns

--- src/main/kotlin/engine/Engine.kt
+++ src/main/kotlin/engine/Engine.kt
@@ -5,18 +5,26 @@ import engine.objectsData.Game
 import engine.reducers.ReducerManager
 import engine.reducers.activationReducers.IActivationReducer
 import engine.reducers.activationReducers.MoveActionReducer
+import engine.reducers.finaleReducers.SoldierCreationReducer
 import engine.reducers.finaleReducers.TurnsReducer
 import engine.reducers.validationReducers.DuplicateActionsReducer
 import engine.reducers.validationReducers.IValidationReducer
 import engine.reducers.validationReducers.MoveValidationReducer
 
 class Engine {
+    // Note!
+    /*
+        Currently, the order of the reducers matter, right now the reducers are invoked this way:
+        Validations - 1,2,3,4... - each validation filters on all the remaining validations
+        Activations - 1,2,3,4... - each activation applies changes of specific action type for all actions
+        Finalizing - 1,2,3,4.... - each finalization applies more changes needed to be done for the next turn
+     */
     fun runTurn(game: Game, actions: List<Action>): Game {
         val validationReducers = listOf(DuplicateActionsReducer(), MoveValidationReducer()) as List<IValidationReducer<Action>>
         val reducerManager = ReducerManager(
             validationReducers,
             listOf(MoveActionReducer() as (IActivationReducer<Action>)),
-            listOf(TurnsReducer())
+            listOf(SoldierCreationReducer(), TurnsReducer())
         )
 
         val validActions = reducerManager.validateState(game, actions)
