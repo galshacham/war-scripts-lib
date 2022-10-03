0e324c9e175e9eb627c42009052b2bf8d44de9b7
galshacham <gal.shacham@gmail.com>
2022-10-03 23:02:23 +0300

    Adding basic BotRunnerInitiator

--- manager/src/test/kotlin/ManagerTests.kt
+++ manager/src/test/kotlin/ManagerTests.kt
@@ -1,12 +1,18 @@
+import botRunner.BotRunnerFactory
+import botRunner.IBotRunner
 import exceptions.NoArgumentsException
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import kotlin.test.Ignore
 import kotlin.test.assertEquals
 
 class ManagerTests {
     @Test
     fun `WHEN initiating manager without players SHOULD throw exception`() {
-        val manager = Manager()
+        val manager = Manager(mockk())
 
         val message = assertThrows<NoArgumentsException> {
             manager.init()
@@ -14,4 +20,26 @@ class ManagerTests {
 
         assertEquals(message, "At least one argument must be assigned")
     }
+
+    @Test
+    fun `GIVEN valid manager WHEN manager init a game with two players SHOULD add two BotRunners`() {
+        val factoryMock = mockk<BotRunnerFactory>()
+
+        val botRunnerMock = mockk<IBotRunner>()
+
+        val botPath1 = "validPath1"
+        val botPath2 = "validPath2"
+
+        every { factoryMock.createBotRunner(botPath1, 0) } returns botRunnerMock
+        every { factoryMock.createBotRunner(botPath2, 1) } returns botRunnerMock
+
+        val manager = Manager(factoryMock)
+
+        manager.init(botPath1, botPath2)
+
+        assertEquals(manager.runners.size, 2)
+        verify { factoryMock.createBotRunner(botPath1, 0) }
+        verify { factoryMock.createBotRunner(botPath2, 1) }
+    }
+
 }
