b67d0262960cf8c9623d15c94d98ddb138effeba
galshacham <gal.shacham@gmail.com>
2022-10-03 23:02:15 +0300

    Adding basic BotRunnerInitiator

--- manager/settings.gradle.kts
+++ manager/settings.gradle.kts
@@ -1,1 +1,2 @@
 rootProject.name = "manager"
+includeBuild("../runner")
--- manager/src/main/kotlin/Manager.kt
+++ manager/src/main/kotlin/Manager.kt
@@ -1,10 +1,18 @@
+import botRunner.BotRunnerFactory
+import botRunner.IBotRunner
 import exceptions.NoArgumentsException
 
-class Manager() {
+class Manager(val runnerFactory: BotRunnerFactory) {
+    val runners = mutableListOf<IBotRunner>()
     fun init(vararg arguments: String) {
         if (arguments.isEmpty()) {
             throw NoArgumentsException()
         }
+
+        arguments.forEachIndexed { i, it ->
+            runners.add((runnerFactory.createBotRunner(it, i)))
+        }
+
     }
 
 }
