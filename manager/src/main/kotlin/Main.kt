b4b991e7011a7aa1b1c6c717dacb5368ee48f2f9
galshacham <gal.shacham@gmail.com>
2022-10-11 13:14:47 +0300

    Just measuring how long it takes for a game It takes about 8 seconds when IdGenerator starts on turn 4 since of a bug which overrides the castles

--- manager/src/main/kotlin/Main.kt
+++ manager/src/main/kotlin/Main.kt
@@ -1,10 +1,13 @@
 import botRunner.BotRunnerFactory
 import kotlinx.serialization.json.Json
 import java.io.File
+import java.util.*
 
 fun main(vararg args: String) {
     val manager = Manager(BotRunnerFactory(Runtime.getRuntime()), Engine(), JsonHandler(Json))
     manager.init(*args)
 
+    println(Date())
     manager.runGame(File("""E:\ComputerScience\Projects\war-scripts-lib\manager\src\test\resources\newDefault.json""").readText())
+    println(Date())
 }
