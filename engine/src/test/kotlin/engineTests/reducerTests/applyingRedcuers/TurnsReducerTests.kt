7a86eab62faeea98f9dea0bd423c529d0b7e7b98
galshacham <gal.shacham@gmail.com>
2022-10-16 18:12:20 +0300

    Fixing TurnsReducerTests

--- engine/src/test/kotlin/engineTests/reducerTests/applyingRedcuers/TurnsReducerTests.kt
+++ engine/src/test/kotlin/engineTests/reducerTests/applyingRedcuers/TurnsReducerTests.kt
@@ -3,9 +3,6 @@ package engineTests.reducerTests.applyingRedcuers
 import reducers.applyingReducers.TurnsReducer
 import objectsData.Game
 import objectsData.GameData
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.slot
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
@@ -13,22 +10,13 @@ import kotlin.test.assertEquals
 class TurnsReducerTests {
     @Test
     fun `WHEN reducer is called SHOULD add turn`() {
-        val game = mockk<Game>()
-
-        val gameData = mockk<GameData>()
-
-        val currentTurn = slot<Int>()
-        val currentTurnValue = 50
-        currentTurn.captured = currentTurnValue
-
-        every { game.gameData } returns gameData
-        every { gameData.currentTurn } returns currentTurnValue
-        every { gameData.currentTurn = capture(currentTurn) } returns Unit
-
         val turnsReducer = TurnsReducer()
+
+        val game = Game(mutableMapOf(), GameData(50, 20))
+        val expectedGame = Game(mutableMapOf(), GameData(50, 21))
 
         turnsReducer.applyState(game)
 
-        assertEquals(currentTurnValue + 1, currentTurn.captured)
+        assertEquals(expectedGame, game)
     }
 }
